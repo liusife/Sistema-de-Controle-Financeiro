167caf781092eb218c0bd414a3d6774495a33911
Miguel Arcanjo <miguelarcanjomartinsilva@gmail.com>
2026-03-25 09:02:47 -0300

    Menu inicial com a função de registro

---
+++ Menuini.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int registro()
+{
+	char nomecom[50];
+	int nu;
+	
+	setlocale(LC_ALL, "Portuguese");
+	printf("Digite o seu nome completo: \n");
+	fgets(nomecom, sizeof(nomecom), stdin);
+	printf("Verifique se seu nome está correto: %s \n", nomecom);
+	printf("1- Sim\n");
+	printf("2- Não\n");
+	scanf("%d", &nu);
+	getchar();
+	
+	if(nu == 2){
+		registro();
+	}
+	printf("Usuário registrado!\n");
+	system("pause");
+	
+	return 0;
+}
+
+int main(int n)
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	do{
+		system("cls");
+		
+	printf("=====MENU=====\n");
+	printf("1- Cadastro\n");
+	printf("2- Login\n");
+	printf("3- Sair\n");
+	scanf("%d", &n);
+		
+	switch(n)
+	{
+	case 1:
+		registro();
+		break;
+	case 2:
+		printf("Você selecionou a opção: %d \n", n);
+		system("pause");
+		break;
+	case 3:
+		printf("\nSaindo...");
+		break;
+	default:
+		printf("\nOpção inválida!\n");
+		system("pause");
+		break;
+		}
+	} while(n != 3);
+	
+	return 0;
+}
